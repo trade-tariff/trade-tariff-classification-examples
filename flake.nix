f6efc2613b4a999551bbfe4a312bd9e77650fbab
William Fish <william.michael.fish@gmail.com>
2026-06-18 16:52:14 +0100

    chore: manage pre-commit hooks with Nix
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -2,6 +2,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
     nixpkgs-ruby = {
       url = "github:bobvanderlinden/nixpkgs-ruby";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,10 +14,11 @@
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      pre-commit-hooks,
       nixpkgs-ruby,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -39,7 +43,67 @@
           "--with-libyaml-lib=${libyaml.out}/lib"
         ];
 
-        init = pkgs.writeShellScriptBin "init" ''cd terraform && terraform init -backend=false'';
+        init = pkgs.writeShellScriptBin "init" "cd terraform && terraform init -backend=false";
+
+        preCommitCheck = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          configPath = ".pre-commit-config-nix.yaml";
+          default_stages = [ "pre-commit" ];
+          hooks = {
+            actionlint = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-yaml = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            deadnix = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            end-of-file-fixer = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            markdownlint = {
+              enable = true;
+              excludes = [ "^terraform/" ];
+              settings.configuration = {
+                MD013 = false;
+              };
+              stages = [ "pre-commit" ];
+            };
+            nixfmt-rfc-style = {
+              package = pre-commit-hooks.inputs.nixpkgs.legacyPackages.${system}.nixfmt;
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            rubocop = {
+              enable = true;
+              name = "rubocop";
+              entry = "bundle exec rubocop --autocorrect";
+              files = "\\.rb$";
+              stages = [ "pre-commit" ];
+            };
+            sort-file-contents = {
+              enable = true;
+              files = ".env.development|.env.test";
+              stages = [ "pre-commit" ];
+            };
+            trim-trailing-whitespace = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            trufflehog = {
+              enable = true;
+              stages = [
+                "pre-commit"
+                "pre-push"
+              ];
+            };
+          };
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -51,9 +115,33 @@
             export PATH=$GEM_HOME/bin:$PATH
 
             export BUNDLE_BUILD__PSYCH="${builtins.concatStringsSep " " psychBuildFlags}"
+            ${preCommitCheck.shellHook}
+            export PATH=${pkgs.writeShellScriptBin "pre-commit" ''
+              set -euo pipefail
+
+              has_config=false
+              for arg in "$@"; do
+                case "$arg" in
+                  -c|--config|--config=*)
+                    has_config=true
+                    ;;
+                esac
+              done
+
+              if [ "$has_config" = true ]; then
+                exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+              fi
+
+              if [ "''${1:-}" = "run" ]; then
+                shift
+                exec ${preCommitCheck.config.package}/bin/pre-commit run --config .pre-commit-config-nix.yaml "$@"
+              fi
+
+              exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+            ''}/bin:$PATH
           '';
 
-          buildInputs = [
+          buildInputs = preCommitCheck.enabledPackages ++ [
             lint
             ruby
             init
